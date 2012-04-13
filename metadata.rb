5c0119f806f6ad466f995d1e5cac483538771362
Andrew Crump <andrew@kotirisoftware.com>
2012-04-13 22:13:43 +0100

    Increment version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs packages for working with XFS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 recipe "xfs", "Installs packages for working with XFS"
 
