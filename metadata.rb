deb4984d99af8a2130236bb70cab7e28917074f5
Tim Smith <tsmith84@gmail.com>
2015-09-08 11:35:45 -0700

    Resolve rubocop warnings

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
-name             "xfs"
-maintainer       "Chef Software, Inc."
-maintainer_email "cookbooks@chef.io"
-license          "Apache 2.0"
-description      "Installs packages for working with XFS"
+name             'xfs'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache 2.0'
+description      'Installs packages for working with XFS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          '1.1.0'
 
-recipe "xfs", "Installs packages for working with XFS"
+recipe 'xfs', 'Installs packages for working with XFS'
 
-%w{ amazon debian ubuntu redhat centos scientific fedora oracle }.each do |os|
+%w( amazon debian ubuntu redhat centos scientific fedora oracle ).each do |os|
   supports os
 end
 
