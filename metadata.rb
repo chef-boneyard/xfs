80030a2642e9361c51c462c75f28ba02033fa6a6
Tim Smith <tsmith84@gmail.com>
2016-02-08 12:26:59 -0800

    Mark as deprecated and release 2.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs packages for working with XFS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 recipe 'xfs', 'Installs packages for working with XFS'
 
